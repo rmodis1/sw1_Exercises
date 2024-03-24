eb9013bd418b34e9178562d812dbf87391e5ef59
Ryan Modisette <modisetteryan@gmail.com>
2024-03-24 17:43:25 -0400

    Added new methods to product logic.

--- ClassExercise1/ProductLogic.cs
+++ ClassExercise1/ProductLogic.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using Product;
+using Products;
 
 namespace Logic
 {
 	internal class ProductLogic
 	{
         private List<Product> _products;
+		private Dictionary<string, DogLeash> _dogLeash;
+		private Dictionary<string, CatFood> _catFood;
+
 
         public ProductLogic()
 		{
@@ -15,14 +18,28 @@ namespace Logic
 			}
 		}
 
-		public void AddProduct(ProductLogic product)
+		public void AddProduct(Product product)
 		{
+			if (product is DogLeash)
+			{
+				_dogLeash.Add(product.Name, product as DogLeash);
+			}
+			if (product is CatFood)
+			{
+				_catFood.Add(product.Name, product as CatFood);
+			}
+
 			_products.Add(product);
 		}
 
 		public List<Product> GetAllProducts()
 		{
 			return _products;
+		}
+
+		public DogLeash GetDogLeashByName(string name)
+		{
+			return _dogLeash[name];
 		}
 	}
 }
