59aaecbaa19e3c7bb2fcc160e3222a7f2908a19c
Ryan Modisette <modisetteryan@gmail.com>
2024-04-02 18:32:08 -0400

    Refactored code to reduce complication in the main method.

--- ClassExercise1/CatFood.cs
+++ ClassExercise1/CatFood.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Logic;
+using Logic; 
 
 namespace Products
 {
@@ -9,6 +11,26 @@ namespace Products
     public class CatFood : Product
     {
         public bool KittenFood { get; set; }
+
+        public static void AddCatFood(CatFood catFood)
+        {
+            Console.WriteLine("Adding new brand of cat food!");
+
+            Console.WriteLine("What is the name of the cat food?");
+            catFood.Name = Console.ReadLine();
+
+            Console.WriteLine("How much does the cat food cost?");
+            catFood.Price = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many bags are you adding?");
+            catFood.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Is this food appropriate for kittens? True/False?");
+            catFood.KittenFood = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please add a short description of the product for customers: ");
+            catFood.Description = Console.ReadLine();
+        }
     }
 }
 
--- ClassExercise1/DogLeash.cs
+++ ClassExercise1/DogLeash.cs
@@ -10,6 +10,29 @@ namespace Products
     {
         public int LengthInches { get; set; }
         public string Material { get; set; }
+
+        public static void AddDogLeash(DogLeash dogLeash)
+        {
+            Console.WriteLine("Adding new dog leash!");
+
+            Console.WriteLine("What is the name of the dog leash?");
+            dogLeash.Name = Console.ReadLine();
+
+            Console.WriteLine("How much does the dog leash cost?");
+            dogLeash.Price = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many leashes are you adding?");
+            dogLeash.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How long is the leash in inches?");
+            dogLeash.LengthInches = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is this leash made out of?");
+            dogLeash.Material = Console.ReadLine();
+
+            Console.WriteLine("Please add a short description of the product for customers: ");
+            dogLeash.Description = Console.ReadLine();
+        }
     }
 }
 
