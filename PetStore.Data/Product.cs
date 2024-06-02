188b358bd5f532fdce65cf84cff4679b48f8b1dc
Ryan Modisette <modisetteryan@gmail.com>
2024-06-02 14:41:17 -0400

    Added folder and path to product context class

--- PetStore.Data/Product.cs
+++ PetStore.Data/Product.cs
@@ -8,6 +8,7 @@ namespace Products
 
     public class Product
     {
+        public int ProductId { get; set; }
         public string? Name { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
--- PetStore.Data/ProductContext.cs
+++ PetStore.Data/ProductContext.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Products;
+
 namespace PetStore.Data
 {
-	public class ProductContext
+	public class ProductContext: DbContext
 	{
+		public DbSet<Product> Products { get; set; }
+
+		public string DbPath { get; }
+
 		public ProductContext()
 		{
+			var folder = Environment.SpecialFolder.LocalApplicationData;
+			var path = Environment.GetFolderPath(folder);
+			DbPath = System.IO.Path.Join(path, "product.db");
 		}
-	}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+			=> options.UseSqlite($"Data Source={DbPath}");
+    }
 }
 
