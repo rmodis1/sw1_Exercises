c9a2a4388c1794b22c42d07aeaa53082ba1fb71d
Ryan Modisette <modisetteryan@gmail.com>
2024-06-02 22:36:24 -0400

    Added a new entity and the relevant logic

---
+++ PetStore.Data/OrderFiles/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PetStore.Data.OrderFiles
+{
+    public interface IOrderRepository
+    {
+        void AddOrder(Order order);
+        Order GetOrder(int id);
+    }
+}
---
+++ PetStore.Data/OrderFiles/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace PetStore.Data.OrderFiles
+{
+	public class OrderRepository: IOrderRepository
+	{
+        private readonly ProductContext _dbContext;
+
+		public OrderRepository()
+		{
+            _dbContext = new ProductContext();
+		}
+
+        public void AddOrder(Order order)
+        {
+            _dbContext.Orders.Add(order);
+            _dbContext.SaveChanges();
+        }
+
+        public Order GetOrder(int id)
+        {
+            return _dbContext.Orders.Include(order => order.Products).SingleOrDefault(order => order.OrderId == id);
+        }
+    }
+}
+
